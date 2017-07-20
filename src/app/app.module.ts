41582c21a630e6a3ac15671912ed0b1aa7e3d4cd
gioeleminardi <gioelem3@gmail.com>
2017-07-20 11:20:15 +0200

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RecipeListComponent} from './recipe/recipe-list/recipe-list.component';
+import {RecipeItemComponent} from './recipe/recipe-item/recipe-item.component';
+import {RecipeDetailComponent} from './recipe/recipe-detail/recipe-detail.component';
+import {ShoppingListComponent} from './shopping/shopping-list/shopping-list.component';
+import {ShoppingListEditComponent} from './shopping/shopping-list-edit/shopping-list-edit.component';
+import {HeaderComponent} from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
