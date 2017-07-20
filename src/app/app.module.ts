d940d986d5578cc5cdcb1f687e1625417ba00a93
gioeleminardi <gioelem3@gmail.com>
2017-07-20 11:39:07 +0200

    layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import {RecipeListComponent} from './recipe/recipe-list/recipe-list.component';
-import {RecipeItemComponent} from './recipe/recipe-item/recipe-item.component';
-import {RecipeDetailComponent} from './recipe/recipe-detail/recipe-detail.component';
-import {ShoppingListComponent} from './shopping/shopping-list/shopping-list.component';
-import {ShoppingListEditComponent} from './shopping/shopping-list-edit/shopping-list-edit.component';
+import {AppComponent} from './app.component';
+import {RecipeListComponent} from './recipes/recipe-list/recipe-list.component';
+import {RecipeItemComponent} from './recipes/recipe-list/recipe-item/recipe-item.component';
+import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {ShoppingListEditComponent} from './shopping-list/shopping-list-edit/shopping-list-edit.component';
 import {HeaderComponent} from './header/header.component';
+import {RecipesComponent} from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import {HeaderComponent} from './header/header.component';
     RecipeDetailComponent,
     ShoppingListComponent,
     ShoppingListEditComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule
@@ -25,4 +27,5 @@ import {HeaderComponent} from './header/header.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
