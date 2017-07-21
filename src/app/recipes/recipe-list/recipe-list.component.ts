42d1b1f026f2ec29fcb70451d0693407b1dc4932
gioeleminardi <gioelem3@gmail.com>
2017-07-21 10:21:34 +0200

    recipe detail layout

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,15 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[];
 
   constructor() {
+    this.recipes = [
+      new Recipe('Test ricetta 1', 'Test descr ricetta 1',
+        'http://pinkitalia.it/wp-content/uploads/2015/05/La-ricetta-del-risotto-al-limone3.jpg'),
+      new Recipe('Test ricetta 2', 'Test descr ricetta 2',
+        'http://www.ilgiornaledelcibo.it/wp-content/uploads/2009/09/risotto-agli-asparagi.jpg')
+    ];
   }
 
   ngOnInit() {
