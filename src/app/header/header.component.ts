f5dad6e13cb83fd3df4abbdd70cf11a5065e3313
gioeleminardi <gioelem3@gmail.com>
2017-07-28 16:30:31 +0200

    added tab switch

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,11 +6,17 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output() navBarTabClickEvent: EventEmitter<string>;
 
   constructor() {
+    this.navBarTabClickEvent = new EventEmitter();
   }
 
   ngOnInit() {
+  }
+
+  onSelect(tab: string) {
+    this.navBarTabClickEvent.emit(tab);
   }
 
 }
