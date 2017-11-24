8e158747fbb24247c1df2462e3b3a51cd3422547
gioeleminardi <gioelem3@gmail.com>
2017-11-24 19:21:50 +0100

    child injector

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,8 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {AppRoutingModule} from './app-routing.module';
 import {HttpModule} from '@angular/http';
-import {RecipeService} from './recipes/recipe.service';
-import {FirebaseService} from './shared/firebase.service';
-import {AuthService} from './auth/auth.service';
-import {AuthGuard} from './auth/auth.guard';
 import {SharedModule} from './shared/shared.module';
 import {ShoppingListModule} from './shopping-list/shopping-list.module';
 import {AuthModule} from './auth/auth.module';
@@ -27,13 +22,7 @@ import {CoreModule} from './core/core.module';
     SharedModule,
     CoreModule
   ],
-  providers: [
-    ShoppingListService,
-    RecipeService,
-    FirebaseService,
-    AuthService,
-    AuthGuard
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -24,7 +24,8 @@ const recipesRoutes: Routes = [
   ],
   exports: [
     RouterModule
-  ]
+  ],
+  providers: [AuthGuard]
 })
 export class RecipesRoutingModule {
 }
