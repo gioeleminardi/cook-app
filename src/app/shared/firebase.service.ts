bc82f994eb415a93999a6f9c76bf97719086ca87
gioeleminardi <gioelem3@gmail.com>
2017-11-25 01:49:37 +0100

    HttpClientModule

--- src/app/shared/firebase.service.ts
+++ src/app/shared/firebase.service.ts
@@ -1,26 +1,25 @@
 import {Injectable} from '@angular/core';
-import {Http, Response} from '@angular/http';
 import {Recipe} from '../recipes/recipe.model';
 import 'rxjs/add/operator/map';
 import {AuthService} from '../auth/auth.service';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable()
 export class FirebaseService {
 
-  constructor(private _http: Http, private _authService: AuthService) {
+  constructor(private _httpClient: HttpClient, private _authService: AuthService) {
   }
 
   saveData(recipes: Recipe[]) {
     const token = this._authService.getToken();
-    return this._http.put('https://varkrid-recipe-book.firebaseio.com/recipes.json?auth=' + token, recipes);
+    return this._httpClient.put('https://varkrid-recipe-book.firebaseio.com/recipes.json?auth=' + token, recipes);
   }
 
   loadData() {
     const token = this._authService.getToken();
-    return this._http.get('https://varkrid-recipe-book.firebaseio.com/recipes.json?auth=' + token)
+    return this._httpClient.get<Recipe[]>('https://varkrid-recipe-book.firebaseio.com/recipes.json?auth=' + token)
       .map(
-        (response: Response) => {
-          const recipes: Recipe[] = response.json();
+        (recipes) => {
           for (const recipe of recipes) {
             if (!recipe['ingredients']) {
               recipe['ingredients'] = [];
