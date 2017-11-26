72180805256ef7fa1fc430398ed2842b63d4c7b8
gioeleminardi <gioelem3@gmail.com>
2017-11-26 02:55:36 +0100

    httprequest + params

--- src/app/shared/firebase.service.ts
+++ src/app/shared/firebase.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {Recipe} from '../recipes/recipe.model';
 import 'rxjs/add/operator/map';
 import {AuthService} from '../auth/auth.service';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams, HttpRequest} from '@angular/common/http';
 
 @Injectable()
 export class FirebaseService {
@@ -12,14 +12,24 @@ export class FirebaseService {
 
   saveData(recipes: Recipe[]) {
     const token = this._authService.getToken();
-    return this._httpClient.put('https://varkrid-recipe-book.firebaseio.com/recipes.json?auth=' + token, recipes);
+    const params = new HttpParams().set('auth', token);
+    // return this._httpClient.put('https://varkrid-recipe-book.firebaseio.com/recipes.json', recipes, {
+    //   params: params
+    // });
+    const req = new HttpRequest('PUT', 'https://varkrid-recipe-book.firebaseio.com/recipes.json', recipes, {
+      reportProgress: true,
+      params: params
+    });
+    return this._httpClient.request(req);
   }
 
   loadData() {
     const token = this._authService.getToken();
-    return this._httpClient.get<Recipe[]>('https://varkrid-recipe-book.firebaseio.com/recipes.json?auth=' + token, {
+    const params = new HttpParams().set('auth', token);
+    return this._httpClient.get<Recipe[]>('https://varkrid-recipe-book.firebaseio.com/recipes.json', {
       observe: 'body',
-      responseType: 'json'
+      responseType: 'json',
+      params: params
     })
       .map(
         (recipes) => {
